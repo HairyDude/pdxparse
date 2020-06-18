4e979b6d8153d076520784a900d4edd85443bec2
Thomas Wood <grand.edgemaster@gmail.com>
2020-06-18 05:01:12 +0100

    EU4: shamanism is now fetishism, use correct icon

--- src/EU4/Handlers.hs
+++ src/EU4/Handlers.hs
@@ -623,6 +623,7 @@ scriptIconTable = HM.fromList
     ,("particularist", "particularists")
     ,("piety", "being pious") -- chosen arbitrarily
     ,("religious_ideas", "religious")
+    ,("shamanism", "fetishism") -- religion reused
     ,("spy_ideas", "espionage")
     ,("tengri_pagan_reformed", "tengri")
     ,("trade_ideas", "trade")
